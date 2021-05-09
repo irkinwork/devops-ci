cd40e00444ff8c2e114d0c2c57de66ccc5857dcd
Irina <irin.work42@gmail.com>
2021-05-09 18:28:50 +0300

    init

---
+++ app/controllers/root.js
@@ -0,0 +1,6 @@
+/* @ts-check */
+
+/* eslint-disable-next-line no-unused-vars */
+export default async (request, response) => {
+
+};
---
+++ app/middlewares/setLocale.js
@@ -0,0 +1,4 @@
+/* @ts-check */
+
+/* eslint-disable-next-line no-unused-vars */
+export default (next, request, response, app) => next();
